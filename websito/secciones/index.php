8ff9c3812648f4e6a9b92da97f1138fffcc834b8
JaviBlaus <120432981+JaviBlaus@users.noreply.github.com>
2023-07-06 19:52:33 -0400

    Update index.php
    Termina inicio y se le agrega color

--- websito/secciones/index.php
+++ websito/secciones/index.php
@@ -3,7 +3,6 @@
 
 <head>
   <title>Title</title>
-  <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -11,14 +10,46 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
+  <!-- Custom CSS -->
+  <style>
+    body {
+      background-color: #F5F5F5;
+      color: #1A120B;
+    }
+
+    .container {
+      background-color: #F2EAD3;
+      border: 1px solid #DFD7BF;
+      padding: 20px;
+    }
+
+    h1 {
+      color: #3F2305;
+    }
+
+    ul li a {
+      color: #141E27;
+    }
+
+    ul li a:hover {
+      color: #203239;
+    }
+  </style>
 </head>
-<h1>Bienvenido al Banco DDJ</h1>
-<p>Selecciona una opción:</p>
-<ul>
-  <li><a href="transacciones.php">Realizar transacciones</a></li>
-  <li><a href="clientes.php">Gestionar clientes</a></li>
-  <li><a href="cuentas.php">Gestionar cuentas</a></li>
-  <li><a href="historial.php">Ver historial</a></li>
-</ul>
-</div>
-<?php include("../templates/pie.php");?>
+
+<body>
+  <div class="container">
+    <h1>Bienvenido al Banco DDJ</h1>
+    <p>Selecciona una opción:</p>
+    <ul>
+      <li><a href="transacciones.php">Realizar transacciones</a></li>
+      <li><a href="clientes.php">Gestionar clientes</a></li>
+      <li><a href="cuentas.php">Gestionar cuentas</a></li>
+      <li><a href="historial.php">Ver historial</a></li>
+    </ul>
+  </div>
+
+  <?php include("../templates/pie.php"); ?>
+</body>
+
+</html>
