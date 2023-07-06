f8195a5850b1d984a16c4819439297664a2a9b80
JaviBlaus <120432981+JaviBlaus@users.noreply.github.com>
2023-07-05 20:09:09 -0400

    Update Historial.php

--- websito/secciones/Historial.php
+++ websito/secciones/Historial.php
@@ -1,3 +1,44 @@
-<?php include("../templates/cabecera.php");?>
-    contenido del historial
-<?php include("../templates/pie.php");?>
+<?php
+include("../templates/cabecera.php");
+include("../configuraciones/bd.php");
+
+$conexionBD = BD::crearInstancia();
+$sql = "SELECT * FROM transacciones";
+$consulta = $conexionBD->prepare($sql);
+if ($consulta->execute()) {
+?>
+
+<div class="container">
+  <h1>Historial de transacciones</h1>
+
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Cuenta a pagar</th>
+        <th>Cuenta a cobrar</th>
+        <th>Monto</th>
+        <th>Fecha</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php while ($row = $consulta->fetch(PDO::FETCH_ASSOC)) : ?>
+        <tr>
+          <td><?php echo $row['id_transa']; ?></td>
+          <td><?php echo $row['cta_paga_transa']; ?></td>
+          <td><?php echo $row['cta_cobra_transa']; ?></td>
+          <td><?php echo $row['monto_transa']; ?></td>
+          <td><?php echo $row['fecha_transa']; ?></td>
+        </tr>
+      <?php endwhile; ?>
+    </tbody>
+  </table>
+</div>
+
+<?php
+} else {
+  echo "Error: " . $consulta->errorInfo()[2];
+}
+
+include("../templates/pie.php");
+?>
