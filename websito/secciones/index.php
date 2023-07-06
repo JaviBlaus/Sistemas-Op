bc9d16e1f4949eba05abe9603c4ea0cb73f658bc
JaviBlaus <120432981+JaviBlaus@users.noreply.github.com>
2023-07-05 20:09:52 -0400

    Update index.php

--- websito/secciones/index.php
+++ websito/secciones/index.php
@@ -1,4 +1,24 @@
-<?php include("../templates/cabecera.php");?>
-    contenido de la pagina
-<?php include("../templates/pie.php");?>
+<!doctype html>
+<html lang="en">
 
+<head>
+  <title>Title</title>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <!-- Bootstrap CSS v5.2.1 -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+<h1>Bienvenido al Banco DDJ</h1>
+<p>Selecciona una opción:</p>
+<ul>
+  <li><a href="transacciones.php">Realizar transacciones</a></li>
+  <li><a href="clientes.php">Gestionar clientes</a></li>
+  <li><a href="cuentas.php">Gestionar cuentas</a></li>
+  <li><a href="historial.php">Ver historial</a></li>
+</ul>
+</div>
+<?php include("../templates/pie.php");?>
